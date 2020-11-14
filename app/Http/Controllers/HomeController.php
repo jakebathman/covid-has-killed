995c012bce49290439d1ef605f1a4e553f5eea14
jakebathman <jake.bathman@gmail.com>
2020-11-13 21:43:00 -0600

    Change to JHU data source

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Place;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
@@ -10,18 +11,21 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $summary = Cache::remember('currentDeaths::us', 300, function () {
-            return collect(Http::get('https://api.covid19api.com/summary')->json()['Countries']);
-        });
-
-        $usa = $summary->where('CountryCode', 'US')->first();
-
-        $currentDeaths = $usa['TotalDeaths'];
+        $currentDeaths = $this->getCurrentDeaths();
         $place = Place::where('population', '<=', $currentDeaths)->orderByDesc('population')->first();
 
         return view('welcome', [
             'place' => $place,
             'currentDeaths' => $currentDeaths,
         ]);
+    }
+
+    protected function getCurrentDeaths()
+    {
+        $url = 'https://jhucoronavirus.azureedge.net/jhucoronavirus/homepage-featured-stats.json';
+
+        return Cache::remember('currentDeaths::us::jhu', 600, function () use ($url) {
+            return Arr::get(Http::get($url)->json(), 'deaths.US');
+        });
     }
 }
